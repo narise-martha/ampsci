b5a6f1a23ec440c423fe42c8e9a7e1dedd8c4bb9
narise-martha <narise.williams@gmail.com>
2025-09-04 10:09:51 +1000

    Adds calculation of reduced matrix element of alpha dot a^(sigma)

--- src/Kionisation/DP_Multipole.cpp
+++ src/Kionisation/DP_Multipole.cpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <memory>
 #include <complex>
+#include <optional>
 
 // RADIAL INTEGRAL FUNCTION
 // int func1 * jL * func2 dr
@@ -37,44 +38,44 @@ double cg(int twola, int twolb, int twoL, int twoma, int twomb, int twoM){
 
 // Matrix elements of a spherical harmonic, Y_{L,M}
 // < lb, mb | Y_{L,M} | la, ma >
-double ME_spherical_harm(int L, int twoM, int lb, int la, int twomb, int twoma) {
+double ME_spherical_harm(int twoL, int twoM, int lb, int la, int twomb, int twoma) {
     
     // Reduced Matrix element
     // < lb || C^L_M || la >
     reduced_ME = pow(-1,lb)*sqrt((2*lb+1)*(2*la+1))*Angular::threej_2(2*lb,2*L,2*la,0,0,0);
 
     // Y_{L,M} = sqrt((2L+1)/4pi) C^L_M
-    return sqrt((2*L+1)/4*M_PI)*pow(-1,lb-mb)*Angular::threej_2(2*lb,2*L,2*la,-twomb,twoM,twoma)*reduced_ME;
+    return sqrt((twoL+1)/4*M_PI)*pow(-1,lb-mb)*Angular::threej_2(2*lb,twoL,2*la,-twomb,twoM,twoma)*reduced_ME;
 }
 
 // Angular integral of the form 
 // C(L,1,J;M-\lambda,\lambda,M) \Omega_b^T Y_{L,M-\lambda} sigma_i \Omega_a
 
 // sigma_x
-double ang_int_sig_x(int L, int twoJ, int twoM, int lambda, int lb, int twojb, int twomb, int la, int twoja, int twoma){
+double ang_int_sig_x(int twoL, int twoJ, int twoM, int lambda, int lb, int twojb, int twomb, int la, int twoja, int twoma){
 
-  double term_1 = cg(2*lb,1,twojb,twomb-1,1,twomb)*cg(2*la,1,twoja,twoma+1,-1,twoma)*ME_spherical_harm(L,twoM,lb,la,twomb-1,twoma+1);
-  double term_2 = cg(2*lb,1,twojb,twomb+1,-1,twomb)*cg(2*la,1,twoja,twoma-1,1,twoma)*ME_spherical_harm(L,twoM,lb,la,twomb+1,twoma-1);
+  double term_1 = cg(2*lb,1,twojb,twomb-1,1,twomb)*cg(2*la,1,twoja,twoma+1,-1,twoma)*ME_spherical_harm(twoL,twoM-2*lambda,lb,la,twomb-1,twoma+1);
+  double term_2 = cg(2*lb,1,twojb,twomb+1,-1,twomb)*cg(2*la,1,twoja,twoma-1,1,twoma)*ME_spherical_harm(twoL,twoM-2*lambda,lb,la,twomb+1,twoma-1);
 
-  return cg(2*L,2,twoJ,twoM-(2*lambda),2*lambda,twoM)*(term_1 + term_2);
+  return cg(twoL,2,twoJ,twoM-(2*lambda),2*lambda,twoM)*(term_1 + term_2);
 }
 
 // sigma_y
-std::complex<double> ang_int_sig_y(int L, int twoJ, int twoM, int lambda, int lb, int twojb, int twomb, int la, int twoja, int twoma){
+std::complex<double> ang_int_sig_y(int twoL, int twoJ, int twoM, int lambda, int lb, int twojb, int twomb, int la, int twoja, int twoma){
 
-  double term_1 = -cg(2*lb,1,twojb,twomb-1,1,twomb)*cg(2*la,1,twoja,twoma+1,-1,twoma)*ME_spherical_harm(L,twoM,lb,la,twomb-1,twoma+1);
-  double term_2 = cg(2*lb,1,twojb,twomb+1,-1,twomb)*cg(2*la,1,twoja,twoma-1,1,twoma)*ME_spherical_harm(L,twoM,lb,la,twomb+1,ma-1);
+  double term_1 = -cg(2*lb,1,twojb,twomb-1,1,twomb)*cg(2*la,1,twoja,twoma+1,-1,twoma)*ME_spherical_harm(twoL,twoM-2*lambda,lb,la,twomb-1,twoma+1);
+  double term_2 = cg(2*lb,1,twojb,twomb+1,-1,twomb)*cg(2*la,1,twoja,twoma-1,1,twoma)*ME_spherical_harm(twoL,twoM-2*lambda,lb,la,twomb+1,ma-1);
   
-  return i*cg(2*L,2,twoJ,twoM-(2*lambda),2*lambda,twoM)*(term_1 + term_2);
+  return i*cg(twoL,2,twoJ,twoM-(2*lambda),2*lambda,twoM)*(term_1 + term_2);
 }
 
 // sigma_z
-double ang_int_sig_z(int L, int twoJ, int twoM, int lambda, int lb, int twojb, int twomb, int la, int twoja, int twoma){
+double ang_int_sig_z(int twoL, int twoJ, int twoM, int lambda, int lb, int twojb, int twomb, int la, int twoja, int twoma){
 
-  double term_1 = cg(2*lb,1,twojb,twomb-1,1,twomb)*cg(2*la,1,twoja,twoma-1,1,twoma)*ME_spherical_harm(L,twoM,lb,la,twomb-1,twoma-1);
-  double term_2 = -cg(2*lb,1,twojb,twomb+1,-1,twomb)*cg(2*la,1,twoja,twoma+1,-1,twoma)*ME_spherical_harm(L,twoM,lb,la,twomb+1,twoma+1);
+  double term_1 = cg(2*lb,1,twojb,twomb-1,1,twomb)*cg(2*la,1,twoja,twoma-1,1,twoma)*ME_spherical_harm(twoL,twoM-2*lambda,lb,la,twomb-1,twoma-1);
+  double term_2 = -cg(2*lb,1,twojb,twomb+1,-1,twomb)*cg(2*la,1,twoja,twoma+1,-1,twoma)*ME_spherical_harm(twoL,twoM-2*lambda,lb,la,twomb+1,twoma+1);
   
-  return cg(2*L,2,twoJ,twoM-(2*lambda),2*lambda,twoM)*(term_1 + term_2);
+  return cg(twoL,2,twoJ,twoM-(2*lambda),2*lambda,twoM)*(term_1 + term_2);
 }
 
 // Need to calculate the angular integral for three different vector spherical harmonic components
@@ -88,7 +89,7 @@ double ang_int_sig_z(int L, int twoJ, int twoM, int lambda, int lb, int twojb, i
 // < b | alpha . jL \vec{Y}_{J,L,M} | a >
 // for a given ma and mb (this will be used to calculate the reduced matrix element)
 
-std::complex<double> ME_JLM(const DiracSpinor &Fa, const DiracSpinor &Fb, const auto jL, int L, int twoJ, int twoM, int twomb, int twoma){
+std::complex<double> ME_Y_JLM(const DiracSpinor &Fa, const DiracSpinor &Fb, const auto jL, int twoL, int twoJ, int twoM, int twomb, int twoma){
 
   // Defining the quantum numbers to feed into the angular integrals
   auto la = Fa.l(); // int
@@ -105,17 +106,83 @@ std::complex<double> ME_JLM(const DiracSpinor &Fa, const DiracSpinor &Fb, const 
   double radial_int_fbga = radial_int(Fb.f(), Fa.g(), jL, grid);
   double radial_int_gbfa = radial_int(Fb.g(), Fa.f(), jL, grid);
 
-  std::complex<double> term1 = (1/sqrt(2))*i*radial_int_fbga*ang_int_sig_x(L,twoJ,twoM,-1,lb,twojb,twomb,l_til_a,twoja,twoma);
-  std::complex<double> term2 = (1/sqrt(2))*radial_int_fbga*ang_int_sig_y(L,twoJ,twoM,-1,lb,twojb,twomb,l_til_a,twoja,twoma);
-  std::complex<double> term3 = radial_int_fbga*ang_int_sig_z(L,twoJ,twoM,0,lb,twojb,twomb,l_til_a,twoja,twoma);
-  std::complex<double> term4 = -(1/sqrt(2))*i*radial_int_fbga*ang_int_sig_z(L,twoJ,twoM,1,lb,twojb,twomb,l_til_a,twoja,twoma);
-  std::complex<double> term5 = (1/sqrt(2))*radial_int_fbga*ang_int_sig_y(L,twoJ,twoM,1,lb,twojb,twomb,l_til_a,twoja,twoma);
+  std::complex<double> term1 = (1/sqrt(2))*i*radial_int_fbga*ang_int_sig_x(twoL,twoJ,twoM,-1,lb,twojb,twomb,l_til_a,twoja,twoma);
+  std::complex<double> term2 = (1/sqrt(2))*radial_int_fbga*ang_int_sig_y(twoL,twoJ,twoM,-1,lb,twojb,twomb,l_til_a,twoja,twoma);
+  std::complex<double> term3 = radial_int_fbga*ang_int_sig_z(twoL,twoJ,twoM,0,lb,twojb,twomb,l_til_a,twoja,twoma);
+  std::complex<double> term4 = -(1/sqrt(2))*i*radial_int_fbga*ang_int_sig_z(twoL,twoJ,twoM,1,lb,twojb,twomb,l_til_a,twoja,twoma);
+  std::complex<double> term5 = (1/sqrt(2))*radial_int_fbga*ang_int_sig_y(twoL,twoJ,twoM,1,lb,twojb,twomb,l_til_a,twoja,twoma);
 
-  std::complex<double> term6 = -(1/sqrt(2))*i*radial_int_gbfa*ang_int_sig_x(L,twoJ,twoM,-1,l_til_b,twojb,twomb,la,twoja,twoma);
-  std::complex<double> term7 = -(1/sqrt(2))*radial_int_gbfa*ang_int_sig_y(L,twoJ,twoM,-1,l_til_b,twojb,twomb,la,twoja,twoma);
-  std::complex<double> term8 = -i*radial_int_gbfa*ang_int_sig_z(L,twoJ,twoM,0,l_til_b,twojb,twomb,la,twoja,twoma);
-  std::complex<double> term9 = (1/sqrt(2))*radial_int_gbfa*ang_int_sig_x(L,twoJ,twoM,1,l_til_b,twojb,twomb,la,twoja,twoma);
-  std::complex<double> term10 = -(1/sqrt(2))*radial_int_gbfa*ang_int_sig_y(L,twoJ,twoM,1,l_til_b,twojb,twomb,la,twoja,twoma);
+  std::complex<double> term6 = -(1/sqrt(2))*i*radial_int_gbfa*ang_int_sig_x(twoL,twoJ,twoM,-1,l_til_b,twojb,twomb,la,twoja,twoma);
+  std::complex<double> term7 = -(1/sqrt(2))*radial_int_gbfa*ang_int_sig_y(twoL,twoJ,twoM,-1,l_til_b,twojb,twomb,la,twoja,twoma);
+  std::complex<double> term8 = -i*radial_int_gbfa*ang_int_sig_z(twoL,twoJ,twoM,0,l_til_b,twojb,twomb,la,twoja,twoma);
+  std::complex<double> term9 = (1/sqrt(2))*radial_int_gbfa*ang_int_sig_x(twoL,twoJ,twoM,1,l_til_b,twojb,twomb,la,twoja,twoma);
+  std::complex<double> term10 = -(1/sqrt(2))*radial_int_gbfa*ang_int_sig_y(twoL,twoJ,twoM,1,l_til_b,twojb,twomb,la,twoja,twoma);
 
   return term1 + term2 + term3 + term4 + term5 + term6 + term7 + term8 + term9 + term10;
 }
+
+
+// < b | alpha . a^(sigma)_{J,M} | a >
+// Where sigma = -1,0,1
+// a^(-1)_{J,M} = sqrt(J/(2J+1)) a_{J,J-1,M} - sqrt((J+1)/(2J+1)) a_{J,J+1,M}
+// a^(0)_{J,M} = a_{J,J,M}
+// a^(1)_{J,M} = sqrt((J+1)/(2J+1)) a_{J,J-1,M} + sqrt(J/(2J+1)) a_{J,J+1,M}
+
+// Using the Wigner-Eckhart theorem, 
+// < b | alpha . a^(sigma)_{J,M} | a > = (-1)^(jb - ja) Wig3j(jb, M, ja; -mb, J, ma) <jb || alpha . a^(sigma)_J || ja >
+
+// Reduced matrix element
+// < jb || alpha . a^(sigma)_J || ja > = (-1)^(ja-jb) [Wig3j(jb, J, ja; -1/2, 0, 1/2)]^{-1} < jb, 1/2 | alpha . a^(sigma)_{J,0} | ja, 1/2 >
+// absolute value squared => |< jb || alpha . a^(sigma)_J || ja >|^2
+double abs_reduced_ME_alpha_a(const DiracSpinor &Fa, const DiracSpinor &Fb, int sigma, int twoJ){
+  
+  std::complex<double> full_ME;
+
+  if (sigma==-1){
+    full_ME = sqrt(twoJ/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ-2,twoJ,0,1,1) - sqrt((twoJ+1)/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ+2,twoJ,0,1,1);
+  }
+
+  elseif (sigma==0){
+    full_ME = ME_Y_JLM(Fa,Fb,jL,twoJ,twoJ,0,1,1);
+  }
+
+  elseif (sigma==-1){
+    full_ME = sqrt((twoJ+1)/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ-2,twoJ,0,1,1) + sqrt(twoJ/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ+2,twoJ,0,1,1);
+  }
+
+  else{
+    return std::nullopt;
+  }
+
+  return pow(abs(pow(-1,0.5*(double(Fa.twoj() - Fb.twoj())))*(1/Angular::threej_2(twojb,twoJ,twoja,-1,0,1))*full_ME),2);
+}
+
+/* 
+std::complex<double> ME_WignerEkhart(){
+
+  std::complex<double> reduced_ME = (1/Angular::threej_2(twojb,twoM,twoja,-1,twoJ,1))*ME_Y_JLM(Fa,Fb,jL,twoL,twoJ,twoM,1,1);
+
+  return Angular::threej_2(twojb,twoM,twoja,-twoma,twoJ,)
+}
+
+// I think all of this is kind of wrong?
+std::complex<double> ME_a_JM(const DiracSpinor &Fa, const DiracSpinor &Fb, int sigma, int twoJ, int twoM){
+
+  if (sigma==-1){
+    return sqrt(twoJ/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ-2,twoJ,twoM,1,1) - sqrt((twoJ+1)/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ+2,twoJ,twoM,1,1);
+  }
+
+  elseif (sigma == 0){
+    return ME_Y_JLM(Fa,Fb,jL,twoJ,twoJ,twoM,1,1);
+  }
+
+  elseif (sigma == 1){
+    return sqrt((twoJ+1)/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ-2,twoJ,twoM,1,1) + sqrt(twoJ/(2*twoJ+2))*ME_Y_JLM(Fa,Fb,jL,twoJ+2,twoJ,twoM,1,1);
+  }
+
+  else{
+    return std::nullopt;
+  }
+}
+
+*/
